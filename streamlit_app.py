f079c6d21d29e6858b87dd615af4083019adff46
Adarsh Pakala <127304185+Adarshpakala2001@users.noreply.github.com>
2024-01-17 11:51:49 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+#title - It is used to add the title
+st.title("Basic App using Streamlit")
+
+# Header
+st.header("Machine Learning")
+
+# Sub header
+st.subheader("linear Regression")
+
+# to give Information
+st.info("Information details of a employee")
+
+#write 
+st.write("Hello World")
